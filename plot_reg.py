fe8a5a4b09250157a41ede17385846898724b093
ljduarte <leo.j.duarte@hotmail.com.br>
2019-07-17 17:01:05 +0100

    updates: 17-07-2019

--- plot_reg.py
+++ plot_reg.py
@@ -12,13 +12,27 @@ coded by L. J. Duarte
 
 import matplotlib.pyplot as plt
 
-def plot_energies(X, wfn_energies, IQA_contributions=[], seg=False, seg_index=[], save = False):
+def standardize(A):
+    avgA = sum(A)/len(A)
+    sqA = [(a-avgA)**2 for a in A]
+    stdevA = (sum(sqA)/(len(A)-1))**(0.5)
+    A = [(a-avgA)/stdevA for a in A]
+    
+    return A
+
+
+def plot_energies(X, wfn_energies, seg=False, seg_index=[], save = False, terms=[], terms_header=[]):
     plt.figure(figsize=(16,8))
     plt.title("REG Analysis")
     plt.xlabel("Control Coordinate")
     plt.ylabel("Energy")
-    plt.plot(X, wfn_energies)
+    plt.plot(X, wfn_energies, color = 'lightgrey', linewidth = 6 )
+    plt.xlim(X[0], X[-1])
     for i in seg_index:
-        plt.axvline(X[i])
+        plt.axvline(X[i], linestyle = 'dotted', color = 'black' )
+    
+    for contribution in terms:
+        plt.plot(X, contribution, linewidth = 2, linestyle = '-.' )
+        
     
     plt.show()
